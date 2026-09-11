14a4808050fd22eafd574773a91a21c23545c22e
nhat251 <nhatnguyendev251@gmail.com>
2026-09-11 16:58:07 +0700

    feat: implement profile management feature with controller, service, and validation

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using APCS.Application.Features.Auth;
+using APCS.Application.Features.Profile;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -19,6 +20,7 @@ public static class DependencyInjection
 
         services.AddValidatorsFromAssembly(assembly);
         services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IProfileService, ProfileService>();
 
         return services;
     }
